6a95b9b90b087dc3c4abc229b932305f6f9b9623
Begzod Safarov <begzod.safarov07@gmail.com>
2023-02-21 09:40:54 +0500

    landing screen and bg improved

--- src/screens/TestScreen.jsx
+++ src/screens/TestScreen.jsx
@@ -5,6 +5,7 @@ import { getWeekIntervals } from '../helpers/dateHelpers'
 
 const TestScreen = () => {
   const [some, setSome] = useState('')
+  const [counter, setCounter] = useState(0)
 
   const onClick = async () => {
     const res = getWeekIntervals(10)
@@ -14,12 +15,17 @@ const TestScreen = () => {
     return { name: 'beck', age: 21 }
   }, [some])
 
+  const increment = () => setCounter((c) => ++c)
+
   return (
     <div style={{ padding: '50px' }}>
       <h1>TestScreen</h1>
-      <button style={{ marginBottom: '30px' }} onClick={onClick}>
-        Click
-      </button>
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <h3>{counter}</h3>
+        <button style={{ marginBottom: '30px' }} onClick={increment}>
+          Increment
+        </button>
+      </div>
       <p>Name: {sampleFunc().name}</p>
       <p>Age: {sampleFunc().age}</p>
       <ProgressBar label='1 year 2 months' />
